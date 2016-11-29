309a962f7eae7919c0dce0e00a34ac6340335ad3
napanagop <nicknfg@gmail.com>
2016-11-29 02:16:48 +0200

    Add directory search and get image size

--- sort_images.py
+++ sort_images.py
@@ -13,11 +13,36 @@ sort_images.py
 
 """
 
-
 import argparse
+import os
+import get_image_size
+
+supported_types = ".png .jpeg .jpg .gif .bmp .ico .tiff .tif"
 
 
 def main():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    args = setup_parser()
+
+    width = -1
+    height = -1
+
+    if not(args.path == ""):
+        dir_path = os.path.join(dir_path, args.path)
+
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            try:
+                width, height = get_image_size.get_image_size(os.path.join
+                                                              (root, file))
+            except get_image_size.UnknownImageFormat:
+                print("Can't get size of "+file+" Propably not an image.")
+            else:
+                print(width, height)
+
+
+def setup_parser():
     parser = argparse.ArgumentParser(description="Move images with dimensions\
                                      smaller than the ones defined to the\
                                      folder <folder> (default: 'sorted').")
@@ -37,7 +62,7 @@ def main():
                         const='sorted', help="name of the folder where detected\
                         images will be moved to")
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
 if __name__ == '__main__':
     main()
