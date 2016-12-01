6f92f3e68c241a492da953ec0deeabca830a4572
napanagop <nicknfg@gmail.com>
2016-12-01 15:41:59 +0200

    Add arguments for more options regarding width and height. Not yet implemented in code.

--- sort_images.py
+++ sort_images.py
@@ -16,6 +16,7 @@ sort_images.py
 import argparse
 import os
 import shutil
+import textwrap
 
 import get_image_size
 
@@ -24,6 +25,8 @@ def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
     args = setup_parser()
+    print(args)
+    return
 
     width = -1
     height = -1
@@ -87,30 +90,27 @@ def main():
 
 
 def setup_parser():
-    parser = argparse.ArgumentParser(description="Move images with dimensions\
-                                     smaller than the ones defined to the\
-                                     folder <folder> (default: 'sorted').")
+    parser = argparse.ArgumentParser(description="""Move images with dimensions smaller than the ones defined to the folder <folder> (default: 'sorted').
+    --width W: Images with width smaller than W will be sorted.
+    --height H: Images with height smaller than H will be sorted.
+    --width W --height H: Images with width smaller than W AND height smaller than H will be sorted.
+    --width W --height H --or: Images with width smaller than W OR height smaller than H will be sorted.""", formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument("width", type=int, help="images with width less than the\
-                        value defined will be moved to the folder <folder>")
+    parser.add_argument("--width", type=int, default="-1", help="images with width less than the value defined will be moved to the folder <folder>")
 
-    parser.add_argument("height", type=int, help="images with height less than\
-                        the value defined will be moved to the folder\
-                        <folder>")
+    parser.add_argument("--height", type=int, default="-1", help="images with height less than the value defined will be moved to the folder <folder>")
+
+    parser.add_argument("--or", action="store_true", help="when providing both width and height arguments use this to make the sorter check if image width OR height is smaller than the defined values")
 
     parser.add_argument("-p", "--path", nargs='?', default='', const='',
-                        help="path to the folder where you want the script to\
-                        scan")
+                        help="path to the folder where you want the script to scan")
 
     parser.add_argument("-f", "--folder", nargs='?', default='sorted',
-                        const='sorted', help="name of the folder where detected\
-                        images will be moved to")
+                        const='sorted', help="name of the folder where detected images will be moved to")
 
-    parser.add_argument("-v", "--verbose", action="store_true", help="increase\
-                         output verosity")
+    parser.add_argument("-v", "--verbose", action="store_true", help="increase output verosity")
 
-    parser.add_argument("-c", "--clean", action="store_true", help="delete the\
-                        sorted pictures")
+    parser.add_argument("-c", "--clean", action="store_true", help="delete the sorted pictures")
 
     return parser.parse_args()
 
