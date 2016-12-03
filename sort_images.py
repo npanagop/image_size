eab58c22cb4161f9fde933a2071b5ad336d637bc
napanagop <nicknfg@gmail.com>
2016-12-03 17:45:17 +0200

    Add comments

--- sort_images.py
+++ sort_images.py
@@ -21,47 +21,64 @@ import get_image_size
 
 
 def main():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
+    # Get arguments
     args = setup_parser()
 
+    # Initialise image width and height
     width = -1
     height = -1
 
+    # Get verbose flag from arguments
     verbFlag = args.verbose
+    # Get clean flag from arguments
     cleanFlag = args.clean
-
-    if not(args.path == ""):
-        dir_path = os.path.join(dir_path, args.path)
-
+    # Get folder where sorted images will be moved to from arguments
     folder = args.folder
+    # Get min width and height from arguments
+    min_width = args.width
+    min_height = args.height
 
+    # Set the path to search
+    if not(args.path == ""):
+        # Get path from arguments
+        dir_path = args.path
+    else:
+        # Get path from where the script was run from
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    # Setup the path where sorted images will be moved to
     sort_path = os.path.join(dir_path, folder)
     if verbFlag:
         print("Making sort folder "+folder+" in "+dir_path)
 
+    # Create the sorted folder if it does not already exist
     try:
         os.mkdir(sort_path)
     except OSError:
         if verbFlag:
             print("Folder already exists")
 
-    min_width = args.width
-    min_height = args.height
-
+    # Check which sort mode will be used based on arguments given
     if (min_width == -1) and (min_height == -1):
+        # No proper arguments where given
         print("Please enter proper arguments.")
         return
     elif min_width == -1:
+        # Sort images only by height
         flagSort = "height"
     elif min_height == -1:
+        # Sort images only by width
         flagSort = "width"
     elif args.either:
+        # Sort images by width OR height
         flagSort = "either"
     else:
+        # Sort images by width AND height
         flagSort = "both"
 
+    # Search the dir_path for images
     for root, dirs, files in os.walk(dir_path):
+        # Skip the folder where sorted images will be moved to
         if root == sort_path:
             if verbFlag:
                 print("Skipping folder "+folder)
@@ -70,13 +87,16 @@ def main():
             print("Searching "+root)
         for file in files:
             valid = False
+            # Get current file's path
             file_path = os.path.join(root, file)
+            # Get current file's width and height, if it's an image
             try:
                 width, height = get_image_size.get_image_size(file_path)
             except get_image_size.UnknownImageFormat:
                 if verbFlag:
                     print("Can't get size of "+file+" Propably not an image.")
             else:
+                # Check the image's size based on the sort mode used
                 if flagSort == "height":
                     if height < min_height:
                         valid = True
@@ -90,15 +110,18 @@ def main():
                     if (width < min_width) and (height < min_height):
                         valid = True
                 if valid:
+                    # Move the image to the predefined folder
                     if verbFlag:
                         print("Moving "+file)
                     shutil.move(file_path, os.path.join(sort_path, file))
 
     if cleanFlag:
+        # Delete sorted folder and its contents
         shutil.rmtree(sort_path)
         if verbFlag:
             print("Deleting folder: "+folder)
     else:
+        # Delete sorted folder if it's empty
         try:
             os.rmdir(sort_path)
             if verbFlag:
